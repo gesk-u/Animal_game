9a061aa66399fc471b450d74b91316d1c92460b0
Amber Ward <161329375+amberward423@users.noreply.github.com>
2025-09-30 11:25:04 +0300

    Update story.py

--- story.py
+++ story.py
@@ -1,7 +1,7 @@
 import textwrap
 
 story = """
-Teresa is a happy zoo keeper and the owner of Happy animal zoo. Matti is sent on a mission by a mysterious and villainous boss who wants to take the endangered zoo animals and sell them as pets.
+Teresa is a happy zoo keeper and the owner of Happy Animal Zoo. Matti is sent on a mission by a mysterious and villainous boss who wants to take the endangered zoo animals and sell them as pets.
 Matti takes 8 of the endangered animals and flies them through multiple airports to take them to their new "owners". 
 Teresa is on the hunt for each and every animal before it is too late. Find the animals in time!
 """
